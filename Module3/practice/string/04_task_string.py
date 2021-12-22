9d7ab8da83750dac97fd6cac57d5cca26ce17dcf
KozlovMark <96468021+KozlovMark@users.noreply.github.com>
2021-12-22 12:16:47 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -4,4 +4,17 @@
 text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
-# TODO: your code here
+i = 0
+symbol = 0
+word = 0
+length_string = len(text)
+
+while i < length_string:
+    if text[i] != " ":
+        symbol += 1
+    else:
+        symbol = 0
+    if symbol == 6:
+        word += 1
+    i += 1
+print("Number of words per line =", word)
