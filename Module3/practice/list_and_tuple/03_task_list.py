ccce8d52d69604084c11f6f9cf88c85c2618aeec
KozlovMark <96468021+KozlovMark@users.noreply.github.com>
2021-12-22 14:40:54 +0300

    Update 03_task_list.py

--- Module3/practice/list_and_tuple/03_task_list.py
+++ Module3/practice/list_and_tuple/03_task_list.py
@@ -1,4 +1,10 @@
 # Дан список из n элементов, заполненный произвольными целыми числами в диапазоне от -10 до 10.
 # Вывести на экран сумму всех элементов.
 
-# TODO: your code here
+number_list = (-10, 5, 9, 6, 10, 6, 5, -2)
+sum = 0
+
+for number in number_list:
+    sum = sum + number
+
+print("Sum =", sum)
